0a7f446d6266d455c17d39613558cec0c3b6003c
LuksAl123 <lucasalprogrammer@gmail.com>
2025-04-28 22:59:09 -0300

    Initial Commit

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+class CardList extends Component {
+
+	render() {
+		const { monsters } = this.props;
+
+		return (
+			<div className='card-list'> 
+				{monsters.map((monsters) => (
+					<h1 key={monsters.id}>{monsters.name}</h1>
+				))}
+			</div>
+		);
+	}
+}
+
+export default CardList;
